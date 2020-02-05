7eb9513f68668fccb4907e2e03315ef8064361a6
MrDent0n <50826182+MrDent0n@users.noreply.github.com>
2020-02-05 22:16:57 +0100

    Update Import_users.ps1

--- User-Import/Import_users.ps1
+++ User-Import/Import_users.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-    Multiselect = $false # om du ønsker å velge flere filer
-	Filter = 'File (*.csv)|*.csv' # filtrerer hvilken fil typer som kan velges
+    Multiselect = $false # if you want to choose more then one file type
+	Filter = 'File (*.csv)|*.csv' # set the file type that you want to be able to import
 }
  
 [void]$FileBrowser.ShowDialog()
@@ -10,33 +10,30 @@ $path = $FileBrowser.FileNames;
 
 If($FileBrowser.FileNames -like "*\*") {
 
-	$FileBrowser.FileNames #Lists selected files (optional)
+	$FileBrowser.FileNames # Lists selected files (optional)
 	
 	foreach($file in Get-ChildItem $path){
 	Get-ChildItem ($file) |
 		ForEach-Object {
-        # importerer modulen activedirectory så vi kan bruke komandoene til å håndtere brukere.
 Import-Module activedirectory
 
 $ADUsers = Import-csv $path
 
  
-foreach ($User in $ADUsers) #loop som kjører gjennom hele lista for alle brukerene
+foreach ($User in $ADUsers) #loop that run the entire file you selected
 {
 		
-	$Username 	= $User.username #brukernavnet til hver bruker
+	$Username 	= $User.username
 	$Password 	= $User.password #her brukes et fast passord til alle
-	$Firstname 	= $User.firstname #fornavn
-	$Lastname 	= $User.lastname #etternavn
-	$OU 		= $User.ou #refererer til hvilken org unit brukeren skal legges i
+	$Firstname 	= $User.firstname
+	$Lastname 	= $User.lastname
+	$OU 		= $User.ou # reference to the org unit the users should be added to
     $email      = $User.email #email adresse
     $jobtitle   = $User.jobtitle
 
-
-	#Sjekker om brukeren allerede eksisterer i domene
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
-		 #hvis brukeren allerede eksisterer i domenet så vil en advarsel bli gitt
+		 # if the user allready exist a warning will be printed
 		 Write-Warning "A user account with username $Username already exist in Active Directory."
 	}
 	else
@@ -56,10 +53,10 @@ foreach ($User in $ADUsers) #loop som kjører gjennom hele lista for alle bruker
             
 	}
 }
-		# Legg til handlinger som skal gjøres i filen
+		# add tasks that you want to run within the file
 		}
 	}
-	# Legg til oppgave som skal kjøres etter at handlingene i filen er fullført
+	# add tasks that run after the file action is complete
 }
 
 else {
